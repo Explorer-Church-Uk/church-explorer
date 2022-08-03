0a8f8078c95e1616606732f2b788b31863bd4242
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-04 00:22:57 +0100

    added users and overseers

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_03_190518) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_03_230259) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "overseers", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_overseers_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "overseers", "users"
 end
