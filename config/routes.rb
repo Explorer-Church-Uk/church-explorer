4db30cdcb3b9e3da58708bec99ae7ecf7af139fe
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-03 23:57:35 +0100

    added home route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'home#index'
+  get '/', controller: :home,action: :index
   get '/about-us', controller: :home, action: :about
   get '/services', controller: :home, action: :services
 end
