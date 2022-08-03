98b5fcce5a930ee7a2b307801b8819c654aa916d
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-04 00:00:20 +0100

    added auth controller

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,24 @@
+class AuthenticationController < ApplicationController
+  before_action :set_user,only: i%[ login ]
+
+  def index
+  end
+
+  # route user to correct user contr
+
+  def login
+    if @user.exists?
+      UserMailer.with(user:@user).deliver_now
+    end
+  end
+
+  # def wedding_login
+  #   redirect_to controller: :wedding,action: :index
+  # end
+
+  private
+
+  def set_user
+    @user = User.find_by params[:email]
+  end
+end
