f7d1724f27800edb42820c09847bd18c1cce358a
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-04 06:31:57 +0100

    new helper functions for the controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,30 @@ class ApplicationController < ActionController::Base
   # after_action :skip_sessions
   after_action :rotate_login_token
 
+  def find_user_by_token
+    User.find_by({token:params[:token]})
+  end
   def rotate_login_token
-    user = User.find_by({token:params[:token]})
+    user = find_user_by_token()
     new_token = SecureRandom.urlsafe_base64
     user.update({token:new_token})
     user.save
     params[:token] = new_token
   end
   def logged_in?(user)
-    user.token == params[:token] || false
+    user.token == params[:token]
   end
+  def is_overseer?
+    Overseer.find_by({user: find_user_by_token()}).exists?
+  end
+  def is_laity?
+    Laity.find_by({user: find_user_by_token()}).exists?
+  end
+  def user_has_wedding?
+    Wedding.find_by(user:find_user_by_token).exists?
+  end
+  def has_wedding_date?
+    Wedding.find_by(user:find_user_by_token).actual_wedding_date.nil? != true
+  end
+  #  TODO - write more functions!
 end
