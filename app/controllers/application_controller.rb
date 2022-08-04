f7c9ead69ef395e68a57b41461f7f693671547fe
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-04 05:56:21 +0100

    new helper functions for the controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
+require 'securerandom'
 class ApplicationController < ActionController::Base
   # needed for submitting forms without cookies
   skip_before_action :verify_authenticity_token
   # after_action :skip_sessions
+  after_action :rotate_login_token
+
+  def rotate_login_token
+    user = User.find_by({token:params[:token]})
+    new_token = SecureRandom.urlsafe_base64
+    user.update({token:new_token})
+    user.save
+    params[:token] = new_token
+  end
+  def logged_in?(user)
+    user.token == params[:token] || false
+  end
 end
