31e0d3f07d0cfac054af0fba78bf862c72827a0b
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-04 01:09:54 +0100

    sessions disabled completely

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  # needed for submitting forms without cookies
   skip_before_action :verify_authenticity_token
-  after_action :skip_session
+  # after_action :skip_sessions
 end
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,2 @@
+# disabled sessions see here - http://www.glitchwrks.com/2017/01/16/removing-cookies-sessions-rails-5
+Rails.application.config.session_store :disabled
