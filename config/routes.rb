9100e21332673e31dc43172b6557798a69b565d0
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-05 00:41:15 +0100

    redirects with params

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :deacons
-  resources :members
-  resources :weddings
-  resources :overseers
-  resources :users
+
+  resources :users do
+    resources :deacons
+    resources :members
+    resources :weddings
+    resources :overseers
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -13,5 +16,7 @@ Rails.application.routes.draw do
   get '/about-us', controller: :home, action: :about
   get '/services', controller: :home, action: :services
   get '/wedding-info', controller: :home, action: :weddings
+  get '/signup/:wedding',to: 'users#new'
+  get '/signup',to: 'users#new'
   get '/login', controller: :authentication, action: :login
 end
