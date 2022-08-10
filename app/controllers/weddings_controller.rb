c0ee737e08638941ce9547785e212ae911e5462a
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-10 06:48:54 +0100

    couple side email complete

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -1,3 +1,4 @@
+require 'vpim/icalendar'
 class WeddingsController < ApplicationController
   before_action :set_wedding, only: %i[ show edit update destroy ]
 
@@ -58,17 +59,56 @@ class WeddingsController < ApplicationController
   end
 
   def request_wedding
-
+    WeddingsMailer.new_wedding(params[:couple_nickname],params[:wedding_date],params[:wedding_time],wedding_ics_file(wedding_date:params,wedding_time:string))
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_wedding
+  # Use callbacks to share common setup or constraints between actions.
+  def set_wedding
       @wedding = Wedding.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    def wedding_params
+  # Only allow a list of trusted parameters through.
+  def wedding_params
       params.require(:wedding).permit(:user_id, :mobile, :proposed_wedding_date, :actual_wedding_date, :prenuptual_appointment)
     end
+
+  def wedding_ics_file(to_pastor=false,wedding_date:string,wedding_time:string,couple_nickname:string)
+    wedding_time = Time.new(wedding_time)
+    wedding_datetime = DateTime.new(wedding_date)
+    wedding_datetime.change(hour:wedding_time.hour,min:wedding_time.min)
+    cal = Vpim::Icalendar.create2
+
+    cal.add_event do |e|
+      e.dtstart       wedding_datetime
+      e.dtend         wedding_datetime
+      e.summary       to_pastor ? "Wedding Date To Confirm" : "Your Preferred Wedding Date"
+      e.description 'A Wedding Date - To Be Confirmed'
+      e.categories    [ 'APPOINTMENT' ]
+      e.categories do |c| c.push 'WEDDING' end
+      e.url           'https://carmelpeasdown.church'
+      e.access_class  "PRIVATE"
+      e.transparency  'OPAQUE'
+
+      now = Time.now
+      e.created       now
+      e.lastmod       now
+
+      e.organizer do |o|
+        o.cn = "Carmel Church - Peasdown"
+        o.uri = "mailto:weddings@carmelpeasdown.church"
+      end
+
+      attendee = Vpim::Icalendar::Address.create("mailto:#{params[:wedding_email]}")
+      attendee.rsvp = false
+      e.add_attendee attendee
+      pastor = Vpim::Icalendar::Address.create("mailto:m.s.cooke@btinternet.com")
+      pastor.rsvp = true
+      e.add_attendee pastor
+    end
+
+    icsfile = File.new("./prefered_wedding_reservatation/#{couple_nickname}-#{wedding_date}-#{wedding_time.strftime('%I-%M')}.ics",'rw')
+    icsfile.write(cal.encode)
+    return icsfile.read()
+  end
 end
--- app/mailers/weddings_mailer.rb
+++ app/mailers/weddings_mailer.rb
@@ -1,2 +1,12 @@
 class WeddingsMailer < ApplicationMailer
+  def new_wedding(wedding_email,couple_nickname,wedding_date,wedding_time,ics_file)
+    attachments['your_proposed_wedding_date.ics'] = ics_file
+    @couple_nickname = couple_nickname
+    @wedding_date = wedding_date
+    @wedding_time = wedding_time
+    mail to: wedding_email,subject: 'Wedding Appointment'
+  end
+  def to_pastor(couple_nickname,wedding_date,wedding_time,ics_file)
+
+  end
 end
