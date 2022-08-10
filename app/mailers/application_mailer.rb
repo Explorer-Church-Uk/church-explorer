d122c3dc6cf506fdb290dfb8290588b46a801aa5
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-10 04:48:50 +0100

    adaptable default mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: ENV['default_email_from_address']
+  layout 'mailer'
 end
