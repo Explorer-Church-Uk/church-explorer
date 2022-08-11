f103db6c748b4943610e0592b4d12576a31f7659
Jason Crockett <JesusisLord33333@outlook.com>
2022-08-11 22:26:23 +0100

    added wedding message to test

--- test/mailers/previews/weddings_mailer_preview.rb
+++ test/mailers/previews/weddings_mailer_preview.rb
@@ -4,6 +4,6 @@ class WeddingsMailerPreview < ActionMailer::Preview
     WeddingsMailer.with(couple_nickname:'weeble and pepper',wedding_phone:'01225462814',wedding_date:'04/12/2022',wedding_time:'12:00',wedding_message:'this is a test',wedding_email:'JesusisLord33333@outlook.com').new_wedding
   end
   def to_pastor
-    WeddingsMailer.with(couple_nickname:'weeble and pepper',wedding_phone:'01225462814',wedding_date:'04/12/2022',wedding_time:'12:00',wedding_message:'this is a test',wedding_email:'JesusisLord33333@outlook.com').to_pastor
+    WeddingsMailer.with(couple_nickname:'weeble and pepper',wedding_phone:'01225462814',wedding_date:'04/12/2022',wedding_time:'12:00',wedding_message:'this is a test',wedding_email:'JesusisLord33333@outlook.com',wedding_message:'this is a test').to_pastor
   end
 end
